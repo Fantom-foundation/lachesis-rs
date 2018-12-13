c4bf633278e5a1fe00f3429a21dd15b090090900
Agustin Chiappe Berrini <jnieve@gmail.com>
2018-12-13 17:28:06 -0500

    Create an implementation of Peer using TCP
    There's a lot of duplicated code from Dummy. There's a
    non-written rule in software development that says once you
    repeat something more than two times, you should abstract it,
    not sooner and not later. This is the second time. If the next
    implementations turns out to need the same infrastructure, it's
    time to abstract the common parts. For now, let's leave it as
    is, as it might be coincidental and I don't want to add
    unnecessary layers of complexity.
    
    Other than that, pretty straight forward once you understand
    Dummy. The only difference is that instead of communicating
    between processes now it communicates through TCP streams.
    
    The network protocol is very simple:
    
    - A connection open to a peer is interpreted as a request to
    sync.
    - On connection open, the peer sends current head and hashgraph
    to the client. In bincode.
    - After that, connection is closed.
    - No other transmission so far.
    
    Right now, that might be sent into multiple tcp packages,
    depending on the hashgraph's size. Over time, that number will
    grow. An easy way to ease this would be to make the
    `HashgraphWire` include just the subset of the graph that is
    still being worked on, as mentioned in the paper. But this pr
    doesn't address that.
    
    This closes #4.

---
+++ tcp-client/src/main.rs
@@ -0,0 +1,38 @@
+use tcp_client::*;
+use std::env::args;
+use std::sync::Arc;
+
+const BASE_PORT: usize = 9000;
+const USAGE: &'static str = "Usage: tcp-client [number of nodes]";
+
+fn main() {
+    env_logger::init();
+    let args: Vec<String> = args().collect();
+    if args.len() != 2 {
+        panic!(USAGE);
+    }
+    let mut rng = ring::rand::SystemRandom::new();
+    let n_nodes = args[1].parse::<usize>().unwrap();
+    let mut nodes = Vec::with_capacity(n_nodes);
+    for i in 0..n_nodes {
+        let a = format!("0.0.0.0:{}", BASE_PORT + i);
+        nodes.push(Arc::new(Box::new(TcpNode::new(&mut rng, a))));
+    }
+    for node in nodes.iter() {
+        for peer in nodes.iter() {
+            if peer.node.get_id() != node.node.get_id() {
+                node.node.add_node(peer.clone()).unwrap();
+            }
+        }
+    }
+    let mut handles = Vec::with_capacity(n_nodes * 2);
+    for node in nodes.iter() {
+        let app = TcpApp(node.clone());
+        let (handle1, handle2) = app.run();
+        handles.push(handle1);
+        handles.push(handle2);
+    }
+    for handle in handles {
+        handle.join().unwrap();
+    }
+}
