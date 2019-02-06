05e609d9ea3c7604b483b2b6ed5714a9a1b71f81
Agustin Chiappe Berrini <jnieve@gmail.com>
2019-02-06 14:19:10 -0500

    VM: Start parsing instructions
    Set the fundations on how to parse the VM.
    
    Change the iterator to not be peekable, as this particular binary code
    doesn't need it.
    
    Change the structure to use `failure::Error` instead of a custom `enum`.
    
    Also, should the `TryFrom` work creating a stream? Maybe, maybe... Let's
    do that later, though.
    
    Signed-off-by: Agustin Chiappe Berrini <jnieve@gmail.com>

--- vm/src/lib.rs
+++ vm/src/lib.rs
@@ -75,14 +75,18 @@ Instructions that may take either a register or constant operand (<Val>) are enc
 or
     <instruction byte> <byte with value 1> <Constant:u64>
 */
+#[macro_use]
+extern crate failure;
+use failure::Error;
 use std::convert::TryFrom;
+use std::iter::Iterator;
 
 pub struct Program(Vec<Instruction>);
 
 pub enum Instruction {
     Fd {
         name: String,
-        args: [Option<u8>; 8],
+        args: u64,
         skip: u64,
     },
     Mov {
@@ -256,15 +260,15 @@ pub enum Instruction {
         value: Value,
     },
     Gg {
-        name: String,
+        string: String,
         register: u8,
     },
     Sg {
-        name: String,
+        string: String,
         register: u8,
     },
     Css {
-        content: String,
+        string: String,
         register: u8,
     },
     CssDyn {
@@ -277,17 +281,197 @@ pub enum Instruction {
     },
 }
 
+macro_rules! parse_instruction_from_register_to_value {
+    ($instr: ident, $stream: ident) => {
+        Instruction::$instr {
+            value: Instruction::parse_value($stream)?,
+            register: Instruction::parse_register($stream)?,
+        }
+    };
+}
+
+macro_rules! parse_instruction_with_string_and_register {
+    ($instr: ident, $stream: ident) => {
+        Instruction::$instr {
+            string: Instruction::parse_string($stream)?,
+            register: Instruction::parse_register($stream)?,
+        }
+    };
+}
+
+impl Instruction {
+    fn parse_fd(stream: &mut Iterator<Item = u8>) -> Result<Instruction, Error> {
+        let name = Instruction::parse_string(stream)?;
+        let args = Instruction::parse_u64(stream)?;
+        let skip = Instruction::parse_u64(stream)?;
+        Ok(Instruction::Fd { name, args, skip })
+    }
+
+    fn parse_mov(stream: &mut Iterator<Item = u8>) -> Result<Instruction, Error> {
+        Ok(parse_instruction_from_register_to_value!(Mov, stream))
+    }
+
+    fn parse_gg(stream: &mut Iterator<Item = u8>) -> Result<Instruction, Error> {
+        Ok(parse_instruction_with_string_and_register!(Gg, stream))
+    }
+
+    fn parse_sg(stream: &mut Iterator<Item = u8>) -> Result<Instruction, Error> {
+        Ok(parse_instruction_with_string_and_register!(Sg, stream))
+    }
+
+    fn parse_css(stream: &mut Iterator<Item = u8>) -> Result<Instruction, Error> {
+        Ok(parse_instruction_with_string_and_register!(Css, stream))
+    }
+
+    fn parse_string(stream: &mut Iterator<Item = u8>) -> Result<String, Error> {
+        let string_length = stream
+            .next()
+            .ok_or(Error::from(ParsingError::StringWithoutLenght))?
+            as usize;
+        let string_bytes: Vec<u8> = stream.take(string_length).collect();
+        let name = String::from_utf8(string_bytes)?;
+        Ok(name)
+    }
+
+    fn parse_u64(stream: &mut Iterator<Item = u8>) -> Result<u64, Error> {
+        let bytes: Vec<u8> = stream.take(8).collect();
+        if bytes.len() == 8 {
+            let mut current_shift: u64 = 7 * 8;
+            Ok(bytes.into_iter().fold(0u64, |acc, n| {
+                let r = acc + ((n as u64) << current_shift);
+                current_shift = current_shift.wrapping_sub(8);
+                r
+            }))
+        } else {
+            Err(Error::from(ParsingError::U64LacksInformation))
+        }
+    }
+
+    fn parse_register(stream: &mut Iterator<Item = u8>) -> Result<u8, Error> {
+        stream
+            .next()
+            .ok_or(Error::from(ParsingError::RegisterExpectedNothingFound))
+    }
+
+    fn parse_value(stream: &mut Iterator<Item = u8>) -> Result<Value, Error> {
+        let flag = stream
+            .next()
+            .ok_or(Error::from(ParsingError::ValueWithNoFlag))?;
+        if flag == 0 {
+            Instruction::parse_register(stream).map(|v| Value::Register(v))
+        } else {
+            Instruction::parse_u64(stream).map(|c| Value::Constant(c))
+        }
+    }
+}
+
 pub enum Value {
     Constant(u64),
     Register(u8),
 }
 
-pub enum ParsingError {}
+#[derive(Debug, Fail)]
+pub enum ParsingError {
+    #[fail(display = "The string being parsed doesn't have lenght")]
+    StringWithoutLenght,
+    #[fail(display = "There weren't enough bytes to form a u64")]
+    U64LacksInformation,
+    #[fail(display = "Type flag not present when parsing value")]
+    ValueWithNoFlag,
+    #[fail(display = "Empty strean when trying to parse a register")]
+    RegisterExpectedNothingFound,
+    #[fail(display = "Invalid instruction byte")]
+    InvalidInstructionByte,
+}
 
 impl TryFrom<Vec<u8>> for Program {
-    type Error = ParsingError;
-    fn try_from(instructions: Vec<u8>) -> Result<Program, ParsingError> {
-        let _peekable = instructions.iter().peekable();
-        Ok(Program(Vec::new()))
+    type Error = Error;
+    fn try_from(instructions: Vec<u8>) -> Result<Program, Error> {
+        let mut peekable = instructions.into_iter();
+        let mut instructions = Vec::new();
+        let mut next = peekable.next();
+        while next.is_some() {
+            let byte = next.expect("can't happen");
+            let instruction = match byte {
+                0x00 => Instruction::parse_fd(&mut peekable),
+                0x01 => Instruction::parse_mov(&mut peekable),
+                0x02 => Instruction::parse_gg(&mut peekable),
+                0x03 => Instruction::parse_sg(&mut peekable),
+                0x04 => Instruction::parse_css(&mut peekable),
+                _ => Err(Error::from(ParsingError::InvalidInstructionByte)),
+            }?;
+            instructions.push(instruction);
+            next = peekable.next();
+        }
+        Ok(Program(instructions))
     }
 }
+/*
+enum Commands : unsigned char {
+    CMD_LD8,
+    CMD_LD16,
+    CMD_LD32,
+    CMD_LD64,
+    CMD_ST8,
+    CMD_ST16,
+    CMD_ST32,
+    CMD_ST64,
+    CMD_LEA,
+
+    CMD_IADD,
+    CMD_ISUB,
+    CMD_SMUL,
+    CMD_UMUL,
+    CMD_SREM,
+    CMD_UREM,
+    CMD_SDIV,
+    CMD_UDIV,
+
+    CMD_AND,
+    CMD_OR,
+    CMD_XOR,
+    CMD_SHL,
+    CMD_LSHR,
+    CMD_ASHR,
+    CMD_INEG,
+
+    CMD_FADD,
+    CMD_FSUB,
+    CMD_FMUL,
+    CMD_FDIV,
+    CMD_FREM,
+
+    CMD_EQ,
+    CMD_NE,
+
+    CMD_SLT,
+    CMD_SLE,
+    CMD_SGT,
+    CMD_SGE,
+    CMD_ULT,
+    CMD_ULE,
+    CMD_UGT,
+    CMD_UGE,
+    CMD_FEQ,
+    CMD_FNE,
+    CMD_FLT,
+    CMD_FLE,
+    CMD_FGT,
+    CMD_FGE,
+    CMD_JMP,
+    CMD_JNZ,
+    CMD_JZ,
+    CMD_CALL0,
+    CMD_CALL1,
+    CMD_CALL2,
+    CMD_CALL3,
+    CMD_CALL4,
+    CMD_CALL5,
+    CMD_CALL6,
+    CMD_CALL7,
+    CMD_CALL8,
+    CMD_RET,
+    CMD_LEAVE,
+    CMD_CSS_DYN,
+};
+*/
