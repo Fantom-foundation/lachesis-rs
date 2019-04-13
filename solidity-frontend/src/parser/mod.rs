b2ceb1fad9855d6bf30e9175d3f6a28c1e3f4fe5
Agustin Chiappe Berrini <jnieve@gmail.com>
2019-04-13 08:29:03 -0400

    More work on the parser
    Signed-off-by: Agustin Chiappe Berrini <jnieve@gmail.com>

--- solidity-frontend/src/parser/mod.rs
+++ solidity-frontend/src/parser/mod.rs
@@ -1,3 +1,129 @@
+pub enum SourceUnit {
+    PragmaDirective(Identifier),
+    ImportDirective(ImportDirective),
+    ContractDefinition(ContractDefinition),
+}
+
+pub struct ContractDefinition {}
+
+pub enum TypeName {
+    Address,
+    AddressPayable,
+    ArrayTypeName(Box<TypeName>, Option<Box<Expression>>),
+    ElementaryTypeName(ElementaryTypeName),
+    FunctionTypeName(FunctionTypeName),
+    Mapping(ElementaryTypeName, Box<TypeName>),
+    UserDefinedTypeName(UserDefinedTypeName),
+}
+
+pub type Block = Vec<Statement>;
+pub enum Statement {
+    Block(Block),
+    Break,
+    Continue,
+    DoWhileStatement(Box<Statement>, Expression),
+    Emit(FunctionCall),
+    ForStatement(SimpleStatement, Expression, Expression, Box<Statement>),
+    IfStatement(IfStatement),
+    InlineAssemblyStatement,
+    PlaceholderStatement,
+    Return(Option<Expression>),
+    SimpleStatement(SimpleStatement),
+    Throw,
+    WhileStatement(Expression, Box<Statement>),
+}
+
+pub enum SimpleStatement {
+    ExpressionStatement(Expression),
+    VariableDefinition(Vec<VariableDeclaration>, Option<Expression>),
+}
+
+pub enum Expression {
+    BinaryExpression(BinaryExpression),
+    FunctionCall(FunctionCall),
+    GroupExpression(Box<Expression>),
+    IndexAccess(Box<Expression>, Box<Expression>),
+    LeftUnaryExpression(LeftUnaryExpression),
+    MemberAccess(Box<Expression>, Identifier),
+    NewExpression(TypeName),
+    PrimaryExpression(PrimaryExpression),
+    RightUnaryExpression(RightUnaryExpression),
+    TernaryOperator(Box<Expression>, Box<Expression>, Box<Expression>),
+}
+
+pub enum ContractType {
+    Contract,
+    Interface,
+    Library,
+}
+
+pub struct InheritanceSpecifier {
+    parent: UserDefinedTypeName,
+    arguments: Vec<Expression>,
+}
+
+pub enum ContractPart {
+    EnumDefinition(EnumDefinition),
+    EventDefinition(EventDefinition),
+    FunctionDefinition(FunctionDefinition),
+    ModifierDefinition(ModifierDefinition),
+    StateVariableDeclaration(StateVariableDeclaration),
+    StructDefinition(StructDefinition),
+    UsingForDeclaration(UsingForDeclaration),
+}
+
+pub struct StructDefinition {}
+
+pub struct ModifierDefinition {}
+
+pub struct FunctionDefinition {}
+
+pub struct EventDefinition {}
+
+pub struct EnumDefinition {}
+
+pub enum UsingForDeclaration {
+    UsingForAll(Identifier),
+    UsingFor(Identifier, TypeName),
+}
+
+pub enum VariableModifier {
+    Constant,
+    Internal,
+    Private,
+    Public,
+}
+
+pub struct StateVariableDeclaration {
+    type_name: TypeName,
+    modifiers: Vec<VariableModifier>,
+    name: Identifier,
+    value: Option<Expression>,
+}
+
+pub struct UserDefinedTypeName {
+    base: Identifier,
+    members: Vec<Identifier>,
+}
+
+pub enum ImportDirective {
+    SimpleImport(String, Option<Identifier>),
+    ImportFrom(Vec<(Identifier, Option<Identifier>)>, String),
+    ImportAllFrom(String, Box<Identifier>),
+}
+
+pub struct IfStatement {
+    condition: Expression,
+    true_branch: Box<Statement>,
+    false_branch: Box<Statement>,
+}
+
+pub struct VariableDeclaration {
+    identifier: Identifier,
+    storage: Option<Storage>,
+    type_name: TypeName,
+}
+
 pub enum LeftUnaryOperator {
     Bang,
     Delete,
@@ -46,18 +172,6 @@ pub enum BinaryOperator {
     StarEquals,
 }
 
-pub enum Expression {
-    BinaryExpression(BinaryExpression),
-    GroupExpression(Box<Expression>),
-    IndexAccess(Box<Expression>, Box<Expression>),
-    LeftUnaryExpression(LeftUnaryExpression),
-    MemberAccess(Box<Expression>, Identifier),
-    NewExpression(TypeName),
-    PrimaryExpression(PrimaryExpression),
-    RightUnaryExpression(RightUnaryExpression),
-    TernaryOperator(Box<Expression>, Box<Expression>, Box<Expression>),
-}
-
 pub struct FunctionCall {
     callee: Identifier,
     arguments: Vec<Expression>,
@@ -65,23 +179,44 @@ pub struct FunctionCall {
 
 pub struct Identifier(String);
 
-pub enum TypeName {
-    ElementaryTypeName(ElementaryTypeName),
+pub enum FunctionModifier {
+    External,
+    Internal,
+    Payable,
+    Pure,
+    View,
+}
+
+pub enum Storage {
+    Calldata,
+    Memory,
+    Storage,
+}
+
+pub struct FunctionTypeName {
+    arguments: Vec<FunctionParameter>,
+    modifiers: Vec<FunctionModifier>,
+    return_values: Vec<FunctionParameter>,
+}
+
+pub struct FunctionParameter {
+    type_name: TypeName,
+    storage: Option<Storage>,
 }
 
 pub struct BinaryExpression {
-    left: Expression,
+    left: Box<Expression>,
     op: BinaryOperator,
-    right: Expression,
+    right: Box<Expression>,
 }
 
 pub struct LeftUnaryExpression {
-    value: Expression,
+    value: Box<Expression>,
     op: LeftUnaryOperator,
 }
 
 pub struct RightUnaryExpression {
-    value: Expression,
+    value: Box<Expression>,
     op: RightUnaryOperator,
 }
 
